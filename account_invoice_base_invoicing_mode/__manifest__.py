2ea97f3ca87f6e88f2beca7e18b186345c5b6718
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2020-07-13 15:18:02 +0200

    Add domain to check invoicing day

--- account_invoice_base_invoicing_mode/__manifest__.py
+++ account_invoice_base_invoicing_mode/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Account Invoice Base Invoicing Mode",
     "version": "13.0.1.0.0",
-    "summary": "Base module for invoicing mode",
+    "summary": "Base module for handling multiple invoicing mode",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-invoicing",
     "license": "AGPL-3",
--- account_invoice_mode_monthly/models/sale_order.py
+++ account_invoice_mode_monthly/models/sale_order.py
@@ -1,7 +1,12 @@
 # Copyright 2020 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
+from datetime import datetime
+
+from dateutil import relativedelta
+
 from odoo import api, models
+from odoo.osv.expression import OR
 
 from odoo.addons.queue_job.job import job
 
@@ -43,8 +48,42 @@ class SaleOrder(models.Model):
                 # ("order_line.qty_to_invoice", ">", 0),
             ]
         )
-        # By default grouped by partner/currency and refund are not generated
+        # By default grouped by partner/currency. Refund are not generated
         invoices = sales._create_invoices(grouped=partner.one_invoice_per_order)
         # Should this be done by another job , probably even if it is only one line
         invoices.action_post()
         return invoices
+
+    @api.model
+    def _company_monthly_invoicing_today(self):
+        """Get company ids for which today is monthly invoicing day."""
+        today = datetime.now()
+        first_day_of_this_month = today.replace(
+            day=1, hour=0, minute=0, second=0, microsecond=0
+        )
+        first_day_of_last_month = first_day_of_this_month - relativedelta.relativedelta(
+            months=1
+        )
+        last_day_of_this_month = (today + relativedelta.relativedelta(day=31)).day
+        # Last month still not executed, it needs to be done
+        domain_last_month = [
+            ("invoicing_mode_monthly_last_execution", "<", first_day_of_last_month),
+        ]
+        # Invoicing day is today or in the past and invoicing not yet done
+        domain_this_month = [
+            ("invoicing_mode_monthly_last_execution", "<", first_day_of_this_month),
+            ("invoicing_mode_monthly_day_todo", "<=", today.day),
+        ]
+        # Make sure non exisiting days are done at the end of the month
+        domain_last_day_of_month = [
+            ("invoicing_mode_monthly_last_execution", "<", first_day_of_this_month),
+            ("invoicing_mode_monthly_day_todo", ">", today.day),
+        ]
+        if today.day == last_day_of_this_month:
+            domain = OR(
+                [domain_last_month, domain_this_month, domain_last_day_of_month]
+            )
+        else:
+            domain = OR([domain_last_month, domain_this_month])
+
+        return self.env["res.company"].search(domain)
