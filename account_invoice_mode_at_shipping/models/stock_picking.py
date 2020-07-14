1b75ee42cc6796f9ba7d90f93b991731b8232ef5
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2020-07-14 07:44:29 +0200

    Add method for invoicing pick

--- account_invoice_mode_at_shipping/models/stock_picking.py
+++ account_invoice_mode_at_shipping/models/stock_picking.py
@@ -10,10 +10,13 @@ class StockPicking(models.Model):
     def action_done(self):
         """  """
         res = super().action_done()
+        self._invoicing_at_shipping()
+        return res
+
+    def _invoicing_at_shipping(self):
         picking_to_invoice = self.filtered(
             lambda r: r.sale_id.partner_invoice_id.invoicing_mode == "at_shipping"
             and r.picking_type_code == "outgoing"
         )
         if picking_to_invoice:
             picking_to_invoice.sale_id._create_invoices()
-        return res
